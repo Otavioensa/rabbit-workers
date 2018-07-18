0383b50751f9733eb5da3e12bcc990fb2eec4a6a
Otavioensa <otavioensa@gmail.com>
2018-07-18 00:24:11 -0300

    achitecture: it creates folders to the pblusher/worker apps

--- src/publisher/index.js
+++ src/publisher/index.js
@@ -1,18 +1,12 @@
 
 import amqp from 'amqplib'
-import {
-  resolve,
-} from 'bluebird'
-import config from './config'
+import { resolve } from 'bluebird'
+import config from '../config'
 
 const assertQueueOptions = { durable: true }
 const sendToQueueOptions = { persistent: true }
 const data = process.argv[2] ? process.argv[2] : 'any data goes here'
-
-const {
-  uri,
-  workQueue,
-} = config
+const { uri, workQueue } = config
 
 const lightTask = () => resolve(console.log('Light task abstraction'))
 
--- src/worker/index.js
+++ src/worker/index.js
@@ -1,17 +1,11 @@
 
 import amqp from 'amqplib'
-import {
-  resolve,
-} from 'bluebird'
-import config from './config'
+import { resolve } from 'bluebird'
+import config from '../config'
 
 const assertQueueOptions = { durable: true }
 const consumeQueueOptions = { noAck: false }
-
-const {
-  uri,
-  workQueue,
-} = config
+const { uri, workQueue } = config
 
 const genRamdomTime = () => Math.random() * 10000
 
@@ -19,7 +13,8 @@ const processHeavyTask = msg => resolve(console.log('Message received'))
   .then(setTimeout(() => console.log(msg.content.toString()), genRamdomTime()))
 
 const assertAndConsumeQueue = (channel) => {
-
+  console.log('Worker is running! Waiting for new messages...')
+  
   const ackMsg = (msg) => resolve(msg)
     .tap(msg => processHeavyTask(msg))
     .then((msg) => channel.ack(msg))
@@ -27,7 +22,7 @@ const assertAndConsumeQueue = (channel) => {
   return channel.assertQueue(workQueue, assertQueueOptions)
     .then(() => channel.prefetch(1))
     .then(() => channel.consume(workQueue, ackMsg, consumeQueueOptions))
-};
+}
 
 const listenToQueue = () => amqp.connect(uri)
   .then(connection => connection.createChannel())
